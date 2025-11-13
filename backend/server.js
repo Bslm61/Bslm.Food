cdf16d0e6f8a9357bf488941b421039e99945f0d
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-13 22:05:06 +0100

    Update global .gitignore

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables (if you have a .env file)
+dotenv.config();
+
+//app config
+const app = express();
+const PORT = process.env.PORT || 4000; //For production or scalable apps --- You mustn't edit code if you ever change port or deploy
+
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+
+// run Express server
+app.listen(PORT, () => {
+  console.log(`✅ Server started on http://localhost:${PORT}`);
+});
