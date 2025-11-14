e3d6e7a6e63a166c0936bf7b1a11843e5104cf33
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-14 20:41:11 +0100

    Create foodRouter

---
+++ backend/routes/foodRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { addFood } from "../controllers/foodController.js"
+import  multer  from "multer"   //create image storage system
+
+
+const foodRouter = express.Router();
+
+
+
+
+
+
+export default foodRouter;
