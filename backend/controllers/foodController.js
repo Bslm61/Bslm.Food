9845bbd95825c6829b8b2e510344770f2151e5a2
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-14 22:29:18 +0100

    test done on postman + image has been added to upload

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -2,30 +2,31 @@ import { log } from "console";
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
+// Controller to handle adding a new food item
 
-// add food item
+const addFood = async (req, res) => {
+  // multer stores the uploaded image in req.file
+  // we save the generated filename to store it in the database
+  let image_filename = `${req.file.filename}`;
 
-const addFood = async (req,res)=> {
-    
-    let image_filename= `${req.file.filename}`;
+  // Create a new food document with data from the form + image
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-    const food = new foodModel({
-
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        category:req.body.category,
-        image:image_filename
-    })
-
-    try {
-        await food.save();
-        res.json({seccess:true,message:"Food Added"})
-    } catch (error) {
-       console.log(error);
-       res.json({succes:false,message:"Error"})
-        
-    }
-}
+  try {
+    // Save the new food item in MongoDB
+    await food.save();
+    res.json({ seccess: true, message: "Food Added" });
+  } catch (error) {
+    console.log(error);
+    // Send a simple error response if something goes wrong
+    res.json({ succes: false, message: "Error" });
+  }
+};
 // we created one addFood controller function
-export {addFood}
+export { addFood };
