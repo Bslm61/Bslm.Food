5a6b1919d456891c049574e31829881917d9a348
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-14 21:33:12 +0100

    postmethod.foodRoutes

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,13 +1,26 @@
-import express from "express"
-import { addFood } from "../controllers/foodController.js"
-import  multer  from "multer"   //create image storage system
-
+import express from "express";
+import { addFood } from "../controllers/foodController.js";
+import multer from "multer"; //middleware used in Express to handle file uploads (images, PDFs, etc.).
 
 const foodRouter = express.Router();
 
+// Image Storage Engine using multer
+// Configure how Multer stores uploaded images on the server
+const storage = multer.diskStorage({
+  destination: "uploads", // Folder where files will be saved
 
+  filename: (req, file, cb) => {
+    // Define how each file will be named when stored
 
+    // Create a unique filename using the current timestamp
+    // Pass the unique file name to Multer
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
+const upload = multer({ storage: storage }); // tells it how to store incoming files & upload becomes a middleware you use in your Express routes.
 
+//post methode to send data on the server , data will be proced and we will get response
+foodRouter.post("/add",upload.single("image"),addFood);
 
 export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // Load environment variables (if you have a .env file)
 dotenv.config();
@@ -16,8 +17,11 @@ app.use(express.json());
 app.use(cors());
 
 // db connection
-
 connectDB();
+
+//APi endpoints
+app.use("/api/food",foodRouter)
+
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
