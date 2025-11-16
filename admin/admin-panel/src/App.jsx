c4aae480075e4ba6dcca04af9da5dcbd222c9af4
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-16 21:45:20 +0100

    admin-panel setup

---
+++ admin/admin-panel/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className='p-0 m-0 box-border font-outfit min-h-screen bg-[#fcfcfc] decoration-0 text-inherit  '>
+
+    </div>
+  )
+}
