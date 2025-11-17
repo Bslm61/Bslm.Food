f69415dca2f2e6358d1bce3367b1180ffacbfd2f
Bslm61 <oussamabouslimane61@gmail.com>
2025-11-17 01:12:05 +0100

    Routes for Add,List & Orders in Sidebar

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 export const Sidebar = () => {
   return (
     <div className="w-20 sm:w-[200px] md:w-[250px] lg:w-[280px] min-h-screen border-r-[1.5px] border-solid border-gray-200 bg-white shadow-md">
       {/* Sidebar Options */}
-      <div className="flex flex-col gap-4 pt-5">
+      <NavLink className="flex flex-col gap-4 pt-5">
         {/* Sidebar Option - Add Items */}
-        <div className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
+        <NavLink to='/add' className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
           <img
             src={assets.add_icon}
             alt="Add Items"
@@ -16,10 +17,10 @@ export const Sidebar = () => {
           <p className="text-gray-700 font-medium text-sm md:text-base group-hover:text-gray-900 hidden sm:block">
             Add Items
           </p>
-        </div>
+        </NavLink>
 
         {/* Sidebar Option - List Items */}
-        <div className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
+        <NavLink to='/list' className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
           <img
             src={assets.order_icon}
             alt="List Items"
@@ -28,10 +29,10 @@ export const Sidebar = () => {
           <p className="text-gray-700 font-medium text-sm md:text-base group-hover:text-gray-900 hidden sm:block">
             List Items
           </p>
-        </div>
+        </NavLink>
 
         {/* Sidebar Option - Orders */}
-        <div className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
+        <NavLink to='/orders' className="mx-3 sm:mx-0 sm:pl-6 md:pl-8 md:ml-6 border border-gray-300 sm:border-r-0 flex items-center justify-center sm:justify-start gap-3 cursor-pointer p-3 rounded-lg sm:rounded-l-lg sm:rounded-r-none hover:bg-gray-50 hover:border-gray-400 transition-all duration-200 group">
           <img
             src={assets.order_icon}
             alt="Orders"
@@ -40,8 +41,8 @@ export const Sidebar = () => {
           <p className="text-gray-700 font-medium text-sm md:text-base group-hover:text-gray-900 hidden sm:block">
             Orders
           </p>
-        </div>
-      </div>
+        </NavLink>
+      </NavLink>
     </div>
   );
 };
